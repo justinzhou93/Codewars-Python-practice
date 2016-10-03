56b622d1645973c44df0d4831dc2c3fd21ae7d8d
justinzhou93 <justinzhou93@gmail.com>
2016-10-03 14:35:58 -0400

    Codewars - Molecule to atoms <3kyu>
    https://www.codewars.com/kata/52f831fa9d332c6591000511
    
    For a given chemical formula represented by a string, count the number of atoms of each element contained in the molecule and return an object.
    
    For example:
    
    water = 'H2O'
    parse_molecule(water)                 # return {H: 2, O: 1}

--- Molecule to atoms.py
+++ Molecule to atoms.py
@@ -11,8 +11,6 @@ def parse_molecule (formula):
     last = ""
     i=0
     while i < len(formula):
-        print formula[i] + ": " + str(i)
-        print counts
         if formula[i].isdigit() == True:
             if i + 1 < len(formula):
                 if formula[i + 1].isdigit() == True:
@@ -23,10 +21,8 @@ def parse_molecule (formula):
             else:
                 counts[last] += int(formula[i]) - 1
         elif formula[i] == ")" or formula[i] == "]" or formula[i] == "}":
-            print 2
             return [i+1, counts]
         elif formula[i] == "(" or formula[i] == "[" or formula[i] == "{":
-            print 3
             temp = parse_molecule(formula[i+1:])
             i += temp[0]
             if i+1 < len(formula):
@@ -36,13 +32,11 @@ def parse_molecule (formula):
                         temp[1][key] = value * int(formula[i])
             counts = combine(temp[1], counts)
         else:
-            print 4
             if formula[i].isupper()==True:
                 if i+1 < len(formula):
-                    print "a"
                     if formula[i+1].islower()==True:
                         if formula[i:i+2] in counts:
-                            counts[formula[i:i+2]] += 1
+                            counts[formula[i:i+2]]+=1
                             last = formula[i:i+2]
                             i+=1
                         else:
@@ -50,86 +44,17 @@ def parse_molecule (formula):
                             last = formula[i:i+2]
                     else:
                         if formula[i] in counts:
-                            counts[formula[i]] += 1
+                            counts[formula[i]] = 1
                             last = formula[i]
                         else:
                             counts[formula[i]] = 1
                             last = formula[i]
                 else:
                     if formula[i] in counts:
-                        counts[formula[i]] += 1
+                        counts[formula[i]] = 1
                         last = formula[i]
                     else:
                         counts[formula[i]] = 1
                         last = formula[i]
         i += 1
     return counts
-
-
-import re
-
-
-def parse_molecule1(formula):
-    f = formula
-    f = re.sub('[\[\{]', '(', f)
-    f = re.sub('[\]\}]', ')', f)
-
-    # Tokenizer
-    n = len(f)
-    a = []
-    i = 0
-    while i < n:
-        if f[i].isupper():
-            a.append(f[i])
-            i += 1
-        elif f[i].islower():
-            a[-1] += f[i]
-            i += 1
-        elif f[i].isdigit():
-            val = 0
-            while i < n and f[i].isdigit():
-                val = val * 10 + ord(f[i]) - ord('0')
-                i += 1
-            a.append(val)
-        else:
-            a.append(f[i])
-            i += 1
-    res = [{}]
-    n = len(a)
-    i = 0
-    while i < n:
-        if re.match(r'^[A-Z][a-z]?$', a[i]):
-            elem = a[i]
-            if i + 1 < n and isinstance(a[i + 1], int):
-                cc = a[i + 1]
-                i += 2
-            else:
-                cc = 1
-                i += 1
-            if not elem in res[-1]:
-                res[-1][elem] = cc
-            else:
-                res[-1][elem] += cc
-        elif a[i] == '(':
-            res.append({})
-            i += 1
-        elif a[i] == ')':
-            if i + 1 < n and isinstance(a[i + 1], int):
-                cc = a[i + 1]
-                i += 2
-            else:
-                cc = 1
-                i += 1
-            for k in res[-1]:
-                if not k in res[-2]:
-                    res[-2][k] = cc * res[-1][k]
-                else:
-                    res[-2][k] += cc * res[-1][k]
-            res.pop()
-    return res.pop()
-
-
-
-print parse_molecule("(C5H5)Fe(CO)2CH3")
-
-
